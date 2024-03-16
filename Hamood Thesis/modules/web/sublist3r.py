4e0fe6e9d171ad9a21a77c46eba80fef3cde7026
El-Diavolo <hamoodalmarhoubi@gmail.com>
2024-03-16 23:28:53 +0400

    1

--- Hamood Thesis/modules/web/sublist3r.py
+++ Hamood Thesis/modules/web/sublist3r.py
@@ -1,15 +1,16 @@
 import subprocess
 import json
 
-subdomainfolder = "results/subdomains/"
+subdomainfolder = "/workspaces/MY-thesis01/Hamood Thesis/results/subdomains/"
 def find_subdomains(domain):
     results = set()
     # Sublist3r
     print("[*] Starting subdomain enumeration with Sublist3r...")
-    subprocess.call(["sublist3r", "-d", domain, "-o", "{subdomainfolder}subdomains_sublist3r.txt"])
+    sublist3r_output_file = f"{subdomainfolder}subdomains_sublist3r.txt"
+    subprocess.call(["sublist3r", "-d", domain, "-o", sublist3r_output_file])
     # Read the output from the file
     try:
-        with open("{subdomainfolder}subdomains_sublist3r.txt", "r") as file:
+        with open(sublist3r_output_file, "r") as file:
             for line in file:
                 results.add(line.strip())
     except FileNotFoundError:
